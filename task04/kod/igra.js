73594c319db59d2c24ccfe484a82df180f915ac8
Ceprnic <ceprnic.josip@gmail.com>
2024-10-29 22:49:11 +0100

    implement task04

---
+++ task04/kod/igra.js
@@ -0,0 +1,137 @@
+const fs = require("fs");
+
+class BaseError extends Error {
+  constructor(status, message) {
+    super(message);
+    this.status = status;
+    this.name = this.constructor.name;
+    Error.captureStackTrace(this);
+  }
+
+  logError() {
+    console.log(this.name);
+    const logMessage = `${new Date().toUTCString()} ${this.status} ${
+      this.name
+    } ${this.stack.replace(/\n/g, " ")}`;
+    fs.appendFileSync("log.txt", logMessage + "\n");
+  }
+}
+
+function swap(arr, index01, index02) {
+  const temp = arr[index01];
+  arr[index01] = arr[index02];
+  arr[index02] = temp;
+}
+
+const DULJINA_STRANICE_PLOCE = 5;
+
+const Igra = function () {
+  const brojMeta = 5;
+
+  let ploca = [];
+  generiraneTocke = {};
+  pogodeneTocke = {};
+  pokusaji = {};
+
+  this.jeLiIgraZavrsena = function () {
+    return Object.keys(pogodeneTocke).length === brojMeta;
+  };
+
+  this.napraviPotez = function (potez) {
+    console.log("Move coordinates:", potez);
+
+    if (this.jeLiIgraZavrsena()) {
+      throw new BaseError(400, "Igra je završena, ne možete napraviti potez.");
+    }
+
+    const key = `${potez.x}-${potez.y}`;
+
+    if (
+      potez.x < 0 ||
+      potez.x >= DULJINA_STRANICE_PLOCE ||
+      potez.y < 0 ||
+      potez.y >= DULJINA_STRANICE_PLOCE
+    ) {
+      throw new BaseError(400, "Potez nije valjan.");
+    }
+
+    if (pokusaji[key]) {
+      throw new BaseError(400, "Potez je već napravljen.");
+    }
+
+    pokusaji[key] = true;
+
+    if (generiraneTocke[key]) {
+      pogodeneTocke[key] = true;
+      if (this.jeLiIgraZavrsena()) {
+        console.log("Igra je završena!");
+      }
+      return true;
+    }
+
+    return false;
+  };
+
+  this.ispisPloce = function () {
+    console.log("Ispis ploce:");
+    for (let x = 0; x < DULJINA_STRANICE_PLOCE; x++) {
+      let line = [];
+      for (let y = 0; y < DULJINA_STRANICE_PLOCE; y++) {
+        let mark = "-";
+        const key = `${x}-${y}`;
+
+        if (pokusaji[key]) {
+          mark = "X";
+        }
+
+        if (pogodeneTocke[key]) {
+          mark = "o";
+        }
+
+        line.push(mark);
+      }
+      console.log(line.join(" "));
+    }
+  };
+
+  function generirajTocke() {
+    const moguceTocke = [];
+    for (let i = 0; i < DULJINA_STRANICE_PLOCE; i++) {
+      for (let j = 0; j < DULJINA_STRANICE_PLOCE; j++) {
+        moguceTocke.push({
+          x: i,
+          y: j,
+        });
+      }
+    }
+
+    for (let i = 0; i < 100; i++) {
+      for (let j = 0; j < 100; j++) {
+        const randomA = Math.floor(Math.random() * 25) % 25;
+        const randomB = Math.floor(Math.random() * 25) % 25;
+
+        swap(moguceTocke, randomA, randomB);
+      }
+    }
+
+    for (let i = 0; i < brojMeta; i++) {
+      const mark = moguceTocke[i];
+      generiraneTocke[`${mark.x}-${mark.y}`] = true;
+    }
+
+    console.log("Generated coordinates:", generiraneTocke);
+  }
+
+  ploca = new Array(DULJINA_STRANICE_PLOCE);
+  for (let i = 0; i < DULJINA_STRANICE_PLOCE; i++) {
+    ploca[i] = new Array(DULJINA_STRANICE_PLOCE);
+  }
+
+  generirajTocke();
+};
+
+module.exports = {
+  Igra,
+  DULJINA_STRANICE_PLOCE,
+  BaseError,
+};
---
+++ task04/kod/index.js
@@ -0,0 +1,101 @@
+const readline = require("readline");
+const fs = require("fs");
+const {
+  Igra: IgraPotapanja,
+  DULJINA_STRANICE_PLOCE,
+  BaseError,
+} = require("./igra");
+
+const novaIgra = new IgraPotapanja();
+
+const questionTekst = `
+Opcije:
+- n          => napravi novu igru
+- p          => ispisi plocu
+- m x:1 y:3 => napravi potez na ovim koordinatama
+- q          => zaustavi proces
+
+Vas odabir: `;
+
+const moveRegex = new RegExp(/m\ x:\d+\ y:\d/);
+
+function formatInput(input) {
+  if (!input) {
+    throw new BaseError(400, "Unos je obavezan.");
+  }
+
+  switch (input) {
+    case "n":
+    case "p":
+    case "q":
+      return { input };
+  }
+
+  if (moveRegex.test(input)) {
+    const splitInputs = input.split(" ");
+    const formattedInput = { input: "m" };
+
+    for (let i = 1; i < 3; i++) {
+      const [key, value] = splitInputs[i].split(":");
+      formattedInput[key] = Number(value);
+    }
+    return formattedInput;
+  }
+  throw new BaseError(400, "Krivi unos");
+}
+
+function inputLoop(igraObj) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.question(questionTekst, (input) => {
+    rl.close();
+
+    try {
+      const formattedInput = formatInput(input);
+      console.log("provjera");
+      console.log(formattedInput);
+
+      switch (formattedInput.input) {
+        case "n":
+          igraObj = new IgraPotapanja();
+          console.log("Nova igra je započeta.");
+          break;
+        case "p":
+          igraObj.ispisPloce();
+          break;
+        case "m":
+          const hit = igraObj.napraviPotez({
+            x: formattedInput.x,
+            y: formattedInput.y,
+          });
+          console.log(hit ? "Pogodak!" : "Promašaj!");
+          if (igraObj.jeLiIgraZavrsena()) {
+            console.log("Igra je završena!");
+          }
+          break;
+        case "q":
+          console.log("Proces je zaustavljen.");
+          process.exit(0);
+      }
+    } catch (err) {
+      if (err instanceof BaseError) {
+        err.logError();
+        console.error(err.message);
+      } else {
+        console.error("Nepoznata greška:", err);
+      }
+    }
+
+    console.log("\n");
+    process.nextTick(() => inputLoop(igraObj));
+  });
+}
+
+function main(igraObj) {
+  inputLoop(igraObj);
+}
+
+process.nextTick(() => main(novaIgra));
